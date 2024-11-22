80a3e163ddac0e5d55ba4d470bc6f2fbfa878011
Joshua Washington <jdwashington195@gmail.com>
2024-11-22 00:11:24 -0500

    Update test_areas.py
    Added tests for rectangle area calculation
    Added test for cricles area calulation

--- test_areas.py
+++ test_areas.py
@@ -1,5 +1,5 @@
 import unittest
-from areas import calculate_rectangle_area
+from areas import calculate_circle_area, calculate_rectangle_area
 
 class TestRectangleArea(unittest.TestCase):
     def test_positive_values(self):
@@ -11,3 +11,14 @@ class TestRectangleArea(unittest.TestCase):
     def test_negative_values(self):
         with self.assertRaises(ValueError):
             calculate_rectangle_area(-5, 10)
+
+class TestCircleArea(unittest.TestCase):
+    def test_positive_radius(self):
+        self.assertAlmostEqual(calculate_circle_area(5), 78.54, places=2)
+
+    def test_zero_radius(self):
+        self.assertEqual(calculate_circle_area(0), 0)
+
+    def test_negative_radius(self):
+        with self.assertRaises(ValueError):
+            calculate_circle_area(-5)
